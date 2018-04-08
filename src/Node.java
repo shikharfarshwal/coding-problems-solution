1a2f780c2f235f43f37f2d6ae0f128de171a43b4
choudharys <shikhar.choudhary@newgen.co.in>
2018-04-08 12:26:35 +0530

    adding linked List implementation

---
+++ src/Node.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Shikhar on 17-02-2018.
+ */
+public class Node {
+
+    String value;
+    Node next;
+
+    Node(String value){
+        this.value=value;
+        next=null;
+    }
+}
