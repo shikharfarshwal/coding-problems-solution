2de1659d1dd251ff5939e18da08aed2d99bb021b
shikhar <shikharfarshwal@outlook.com>
2024-05-31 08:57:25 +0530

    #Day 4 Largest sum contigous subarray

--- src/com/problems/fourfifty/LargestSumContigousArray.java
+++ src/com/problems/fourfifty/LargestSumContigousArray.java
@@ -1,8 +1,15 @@
-package com.problems;
+package com.problems.fourfifty;
 
 import java.util.Scanner;
 
-public class KadaneAlgorithm {
+/*
+To find the largest sum of a contiguous subarray, we can use Kadane's Algorithm.
+This algorithm efficiently solves the problem in O(n) time complexity and is simple to implement.
+The idea is to iterate through the array, keeping track of the maximum sum of the subarray that ends at the current position and the overall maximum sum found so far.
+
+int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+ */
+public class LargestSumContigousArray {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("enter array size");
@@ -12,7 +19,7 @@ public class KadaneAlgorithm {
         for (int e = 0; e <= n - 1; e++) {
             array[e] = scanner.nextInt();
         }
-        System.out.println(findMaxSumSubArray(array));
+        System.out.println(findMaxSumSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
     }
 
     private static int findMaxSumSubArray(final int[] array) {
