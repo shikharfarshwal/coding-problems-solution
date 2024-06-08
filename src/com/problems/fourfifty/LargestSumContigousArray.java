2206bed3d1766ef6e198b1e281f7d7836ef30da4
shikhar <shikharfarshwal@outlook.com>
2024-06-08 14:38:06 +0530

    #Day 5 Grab interview q2 optimized solution + Largest sum contigous su5array

--- src/com/problems/fourfifty/LargestSumContigousArray.java
+++ src/com/problems/fourfifty/LargestSumContigousArray.java
@@ -32,6 +32,4 @@ public class LargestSumContigousArray {
         }
         return max_global;
     }
-
-
 }
---
+++ src/com/problems/fourfifty/LargestSumContigousArrayLessThenK.java
@@ -0,0 +1,49 @@
+package com.problems.fourfifty;
+
+public class LargestSumContigousArrayLessThenK {
+
+    public static void main(String[] args) {
+        System.out.println(maxSubArraySumWithK(new int[]{5, 4, -1, 7, 8}, 3));
+        System.out.println(maxSubArraySumWithKOptimized(new int[]{5, 4, -1, 7, 8}, 3));
+    }
+
+    /*
+      Brute force
+       */
+    public static int maxSubArraySumWithK(int[] arr, int k) {
+        int max_sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int max_current = arr[i];
+            for (int j = i + 1; j < arr.length; j++) {
+                max_current = Math.max(max_sum, max_current + arr[j]);
+
+                if (max_current > max_sum && max_current <= k) {
+                    max_sum = max_current;
+                }
+            }
+        }
+        return max_sum;
+    }
+
+
+    /*
+Q1 Largest sum of a contigous sub array (kadane algorithm)
+Q2 Same question but the sum shouldn't be greater then k
+Had 3 test cases one test case was unable to solve {5,4,-1,7,8} , k=3
+ */
+    //      Had 3 test cases one test case was unable to solve {5,4,-1,7,8} , k=3
+    private static int maxSubArraySumWithKOptimized(final int[] array, int k) {
+        int max_global = 0;
+        for (int i = 0; i <= array.length - 1; i++) {
+            int max_current = array[i];
+            int j = i + 1;
+            if (j < array.length) {
+                max_current = Math.max(max_global, array[j] + max_current);
+                if (max_current > max_global && max_current <= k) {
+                    max_global = max_current;
+                }
+            }
+        }
+        return max_global;
+    }
+}
