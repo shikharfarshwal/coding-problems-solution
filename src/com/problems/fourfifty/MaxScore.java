e55ae7652283e4011ccc90336851393430dd66cc
shikhar <shikharfarshwal@outlook.com>
2024-07-22 10:47:02 +0530

    #Day 7 MAX SCORE FOR CARDS + AGODA INTERVIEW QUESTION

---
+++ src/com/problems/fourfifty/MaxScore.java
@@ -0,0 +1,29 @@
+package com.problems.fourfifty;
+
+/*
+https://leetcode.com/problems/maximum-points-you-can-obtain-from-cards/
+ */
+public class MaxScore {
+
+    public static void main(String[] args) {
+        MaxScore maxScore = new MaxScore();
+        System.out.println(maxScore.maxScore(new int[]{1, 79, 80, 1, 1, 1, 200, 1}, 3));
+    }
+
+    public int maxScore(int[] cardPoints, int k) {
+        int max_sum = 0, lsum = 0, rsum = 0;
+        int r = cardPoints.length - 1;
+
+        for (int i = 0; i < k; i++) {
+            lsum = lsum + cardPoints[i];
+        }
+        max_sum = lsum;
+        for (int i = k - 1; i >= 0; i--) {
+            lsum = lsum - cardPoints[i];
+            rsum = rsum + cardPoints[r];
+            r--;
+            max_sum = Math.max(max_sum, lsum + rsum);
+        }
+        return max_sum;
+    }
+}
---
+++ src/com/problems/interview/Agoda.java
@@ -0,0 +1,50 @@
+package com.problems.interview;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+// need to find 3 elemets in an array that sum up to 0 (a+b+c)=0
+// duplicate elements can also be there
+// array size can be zero
+// min 3 elements shall be there
+// [-7, 11,40,3,1,-4]
+public class Agoda {
+
+    public static boolean solution(List<Integer> num) {
+
+        if (num.size() == 0) {
+            return false;
+        }
+        // Write your code here
+        Integer[] nums = num.toArray(new Integer[0]);
+
+        Arrays.sort(nums);
+
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            while (left < right) {
+                //nums.get(i)
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    return true;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+                // -1, -1, -1, 0, 1, 1
+            }
+        }
+        return false;
+    }
+
+}
+}
